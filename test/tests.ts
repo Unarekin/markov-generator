8dc36446ef6e39435c32706b491b6458925701bf
Erica Phelps <erica@blackspork.com>
2020-11-30 18:04:00 -0600

    Basic tests

--- test/tests.ts
+++ test/tests.ts
@@ -3,16 +3,32 @@ import * as path from 'path';
 import { MarkovGenerator } from '../src';
 let fs = require('fs').promises;
 
-describe("Markov Generator Tests", () => {
-  it("Lorem Ipsum", async () => {
-    let data: string = (await fs.readFile(path.join(__dirname, "data/lorem-ipsum.txt"))).toString();
+describe("Markov Generator Tests", async () => {
 
-    let generator: MarkovGenerator = new MarkovGenerator(" ", data);
-    let items: string[] = generator.generate(10) as string[];
+  function generateTest(file: string): () => Promise<void> {
+    return async function (): Promise<void> {
+      return new Promise(async (resolve, reject) => {
+        try {
+          let data: string = (await fs.readFile(path.join(__dirname, "data", file))).toString();
 
-    assert.isOk(items);
-    assert.isArray(items);
-    assert.property(items, "length");
-    assert.isAtLeast(items.length, 1);
-  })
-})
+          let generator: MarkovGenerator = new MarkovGenerator(data.split(/\r?\n/).filter((phrase: string) => phrase.length));
+          let items: string[] = generator.generate(10) as string[];
+          assert.isOk(items);
+          assert.isArray(items);
+          assert.property(items, "length");
+          assert.equal(items.length, 10);
+
+          resolve();
+        } catch (err) {
+          reject(err);
+        }
+      });
+    }
+  };
+
+
+  it("Lorem Ipsum", generateTest("lorem-ipsum.txt"));
+  it("Bacon Ipsum", generateTest("bacon-ipsum.txt"));
+  it("Trump Ipsum", generateTest("trump-ipsum.txt"));
+  it("Cupcake Ipsum", generateTest("cupcake-ipsum.txt"));
+});
